d5d41b7a5725853b7a1819a906a1b10c56609cdd
Jared Bilton <jsbilton@gmail.com>
2016-10-18 11:49:50 -0400

    modularized components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
 const h = require('react-hyperscript')
+var items = [
+    {
+        id: 1,
+        title: "JavaScript Fatigue",
+        link: "https://hackernoon.com/how-it-feels-to-learn-javascript-in-2016-d3a717dd577f#.abgpmgte7",
+        score: 0
+    }
+]
+const Header = require('./components/Header.js')
+const List = require('./components/List.js')
+const Form = require('./components/Form.js');
+const props = { items: items} //could just use items in List
 
-module.exports = () => h('div', 'Hello Mars')
+module.exports = () => h('div.pa4.bg-blue.vh-100', [
+    Header(),
+    h('main', [
+      List(props),
+      Form()
+    ])
+])
---
+++ src/components/Form.js
@@ -0,0 +1,16 @@
+const h = require('react-hyperscript')
+
+module.exports = _ => h('section.form', [
+    h('h2.f4.white', 'Add News'),
+    h('form', [
+        h('div.pb2', [
+            h('label', 'Title'),
+            h('input')
+        ]),
+        h('div.pb2', [
+            h('label', 'Link'),
+            h('input')
+        ]),
+        h('div.tr', [h('button', 'Add News')])
+    ])
+])
